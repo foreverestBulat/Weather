5cb3dac7654a534b3f0fd623339b7ba1a051febc
bulat <bulatsubuh@gmail.com>
2025-05-27 17:10:55 +0300

    + docker

--- src/config.py
+++ src/config.py
@@ -1,14 +1,11 @@
 import os
 import pathlib
 
-from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse
 
 from dotenv import load_dotenv
 
-os.environ.clear()
+# os.environ.clear()
 load_dotenv()
 
 
@@ -28,16 +25,17 @@ DB_POSTGRESQL = DATABASES['PostgreSQL']
 ASYNC_DB_POSTGRESQL_URL = DATABASE_URL = f'postgresql+asyncpg://{DB_POSTGRESQL["USER"]}:{DB_POSTGRESQL["PASSWORD"]}@{DB_POSTGRESQL["HOST"]}/{DB_POSTGRESQL["NAME"]}'
 SYNC_DB_POSTGRESQL_URL = DATABASE_URL = f'postgresql+psycopg2://{DB_POSTGRESQL["USER"]}:{DB_POSTGRESQL["PASSWORD"]}@{DB_POSTGRESQL["HOST"]}/{DB_POSTGRESQL["NAME"]}'
 
-
+# print('--------------------------------ASYNC')
+# print(ASYNC_DB_POSTGRESQL_URL)
+# print(SYNC_DB_POSTGRESQL_URL)
+# print('---------------------------------SYNC')
 
 REDIS_URL = os.environ.get('REDIS_URL')
-
+# print(REDIS_URL)
+# print(os.getenv('REDIS_URL'))
+# print('--------------------------------REDIS')
 
 BASE_DIR = pathlib.Path(__file__).parent
 templates = Jinja2Templates(directory=[
     BASE_DIR / 'web' / 'templates',
 ])
-
-# print(templates)
-# print(DATABASES)
-# print(OPENMETEO_API_URL)
--- src/main.py
+++ src/main.py
@@ -1,16 +1,15 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
-
-from infrastructure.openmeteo import OpenMeteoService
+from fastapi import FastAPI
 from web.weather_router import weather_router
 from api.city_router import city_router
 
 from persistence.tables import *
 
+
 app = FastAPI()
-# app.mount('/static', StaticFiles(directory='static'), name='static')
 
 app.include_router(weather_router, prefix='/web')
-
 app.include_router(city_router, prefix='/api')
+
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run("main:app", host="127.0.0.1", port=8000)
